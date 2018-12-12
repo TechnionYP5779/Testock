473ca2e1d40b7bdff74d46a7d1699b6ecfe14936
Ron Marcovich <ron4548@gmail.com>
2018-12-12 13:26:23 +0200

    closes #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCREv45Llml_gg4hwXeMTXrB6gsDdBkSUQ',
+    authDomain: 'yp5779-testock.firebaseapp.com',
+    databaseURL: 'https://yp5779-testock.firebaseio.com',
+    projectId: 'yp5779-testock',
+    storageBucket: 'yp5779-testock.appspot.com',
+    messagingSenderId: '997206110546'
+  }
 };
 
 /*
