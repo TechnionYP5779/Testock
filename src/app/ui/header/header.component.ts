f959d37970f91a60011d6e5281c1196b5a1d98cf
guyme <guy.menhel@gmail.com>
2018-12-15 19:09:05 +0200

    changed names of props

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -7,16 +7,16 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   main_menu_opened: boolean;
-  @Output() menu_triggered: EventEmitter<boolean>;
+  @Output() main_menu_triggered: EventEmitter<boolean>;
 
   constructor() {
     this.main_menu_opened = false;
-    this.menu_triggered = new EventEmitter();
+    this.main_menu_triggered = new EventEmitter();
   }
 
   trigger_menu() {
     this.main_menu_opened = !this.main_menu_opened;
-    this.menu_triggered.emit(this.main_menu_opened);
+    this.main_menu_triggered.emit(this.main_menu_opened);
   }
 
   ngOnInit() { }
