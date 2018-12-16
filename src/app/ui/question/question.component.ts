05fa78f7405346d2b42d0e3777b56bd9245ae7d9
Ron Marcovich <ron4548@gmail.com>
2018-12-16 20:02:06 +0200

    defined question component content

--- src/app/ui/question/question.component.ts
+++ src/app/ui/question/question.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {DbService} from '../../core/db.service';
+import {Question} from '../../core/entities/question';
+import {Solution} from '../../core/entities/solution';
 
 @Component({
   selector: 'app-question',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  private id: string;
+  public question: Question;
+  public solutions: Solution[];
+
+  constructor(private route: ActivatedRoute, private db: DbService) {
+    this.id = this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit() {
+    this.getQuestion();
+    this.getSolutions();
+  }
+
+  getQuestion(): void {
+    this.db.getQuestion(this.id).subscribe(q => this.question = q);
+
+  }
+
+  getSolutions(): void {
+    this.db.getSolutions(this.id).subscribe(sol => this.solutions = sol);
   }
 
 }
