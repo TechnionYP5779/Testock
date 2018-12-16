6d219e288501ca6ad333c2379eb83c94663a6c43
Ron Marcovich <ron4548@gmail.com>
2018-12-16 19:45:00 +0200

    added routing to question component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {UserLoginComponent} from './users/user-login/user-login.component';
 import {UserProfileComponent} from './users/user-profile/user-profile.component';
 import {CoursesComponent} from './ui/courses/courses.component';
 import {CourseComponent} from './ui/course/course.component';
+import {QuestionComponent} from './ui/question/question.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/profile', pathMatch: 'full'},
-  { path: 'login', component: UserLoginComponent},
-  { path: 'profile', component: UserProfileComponent},
-  { path: 'courses', component: CoursesComponent},
-  { path: 'course/:id', component: CourseComponent }
+  {path: '', redirectTo: '/profile', pathMatch: 'full'},
+  {path: 'login', component: UserLoginComponent},
+  {path: 'profile', component: UserProfileComponent},
+  {path: 'courses', component: CoursesComponent},
+  {path: 'course/:id', component: CourseComponent},
+  {path: 'question/:id', component: QuestionComponent}
 ];
 
 @NgModule({
-  exports: [ RouterModule],
-  imports: [ RouterModule.forRoot(routes) ],
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
