6de167100961dc3b3f5a3c2695f11e42383edf2c
Ron Marcovich <ron4548@gmail.com>
2018-12-17 13:40:16 +0200

    defined basic function for pdf generation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,20 @@
 import * as functions from 'firebase-functions';
-import * as pdfkit from 'pdfkit'
+import * as PDFDocument from 'pdfkit'
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello")
+export const getPDFofExam = functions.https.onRequest((request, response) => {
+
+  let examId = request.query.examId;
+  console.log(examId);
+  const doc = new PDFDocument;
+
+  doc.pipe(response);
+  doc.addPage()
+    .fontSize(25)
+    .text('Here is some vector graphics...', 100, 100);
+
+  doc.end()
+
 });
