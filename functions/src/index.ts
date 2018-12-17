e76454ebee338bde47b380dc64bfbce14d25e74d
Ron Marcovich <ron4548@gmail.com>
2018-12-17 13:12:41 +0200

    Added cloud functions

---
+++ functions/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+import * as pdfkit from 'pdfkit'
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello")
+});
