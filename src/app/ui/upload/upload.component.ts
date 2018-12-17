4d1e136ddf89b94eccacdf27f476d8e1590ba03c
Ron Marcovich <ron4548@gmail.com>
2018-12-17 23:40:21 +0200

    added automatic course data fetching according to scan file name

--- src/app/ui/upload/upload.component.ts
+++ src/app/ui/upload/upload.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {DbService} from '../../core/db.service';
 import {PdfService} from '../../pdf.service';
 import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {Course} from '../../core/entities/course';
 
 @Component({
   selector: 'app-upload',
@@ -16,11 +17,16 @@ export class UploadComponent implements OnInit {
 
   constructor(private db: DbService, private pdf: PdfService, private sanitizer: DomSanitizer) { }
 
+  public course: Course;
+
   ngOnInit() {
   }
 
   onFileSelected() {
     const file = this.file.nativeElement.files[0];
+
+    this.tryGetCourseDetails(file);
+
     this.pdf.getImagesOfFile(file).then(res => this.images = res.map(img => {
       const url = URL.createObjectURL(img);
       return this.sanitizer.bypassSecurityTrustUrl(url);
@@ -29,5 +35,13 @@ export class UploadComponent implements OnInit {
 
   removePage(img: SafeUrl) {
     this.images.splice(this.images.indexOf(img), 1);
+  }
+
+  private tryGetCourseDetails(file: File) {
+    const fileName = file.name;
+    const split = fileName.split('-');
+    const courseId = parseInt(split[2], 10);
+    this.db.getCourse(courseId).subscribe(course => this.course = course);
+
   }
 }
