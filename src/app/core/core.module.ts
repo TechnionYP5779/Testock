25556c020e19dbae5f86eaa84d794e124b0c2fb6
guyme <guy.menhel@gmail.com>
2018-12-18 22:26:58 +0200

    Added pipe to semester from number to string

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SemesterPipe } from './semester.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [SemesterPipe],
   imports: [
     CommonModule
   ]
---
+++ src/app/core/semester.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'semester'
+})
+export class SemesterPipe implements PipeTransform {
+
+  transform(value: number, args?: any): string {
+    switch (value) {
+      case 1: return 'Winter';
+      case 2: return 'Spring';
+      case 3: return 'Summer';
+      default: return 'Unknown semester';
+    }
+  }
+
+}
