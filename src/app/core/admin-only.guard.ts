4530aa83d07aa90f86566c6af7e881e1787cd4c1
Ron Marcovich <ron4548@gmail.com>
2019-01-08 19:42:18 +0200

    added admin only guard

---
+++ src/app/core/admin-only.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from './auth.service';
+import {map, take, tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminOnlyGuard implements CanActivate {
+  constructor(private auth: AuthService) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.auth.user$.pipe(
+      take(1),
+      map(user => user && user.roles.admin),
+      tap(canView => {
+        if (!canView) {
+          console.error('Access denied.');
+        }
+      })
+    );
+  }
+}
