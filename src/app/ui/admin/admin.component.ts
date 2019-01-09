21ce3a9c1cda7a3e178230fad41463d7f76a1d7e
Ron Marcovich <ron4548@gmail.com>
2019-01-09 15:36:09 +0200

    added option to create new courses

--- src/app/ui/admin/admin.component.ts
+++ src/app/ui/admin/admin.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Roles, UserData} from '../../core/entities/user';
 import {DbService} from '../../core/db.service';
 import {MatSnackBar} from '@angular/material';
+import {FacultyId} from '../../core/entities/faculty';
+import {Course} from '../../core/entities/course';
 
 @Component({
   selector: 'app-admin',
@@ -11,9 +13,12 @@ import {MatSnackBar} from '@angular/material';
 export class AdminComponent implements OnInit {
 
   users: UserData[];
+  faculties: FacultyId[];
+  newCourse: Course = {id: null, faculty: null, name: null};
 
   constructor(private db: DbService, private snackBar: MatSnackBar) {
     this.db.getAllUsers().subscribe(users => this.users = users);
+    this.db.getFaculties().subscribe(facs => this.faculties = facs);
   }
 
   ngOnInit() {
@@ -32,5 +37,12 @@ export class AdminComponent implements OnInit {
     this.db.setUserRoles(user.uid, roles).then(() => {
       this.snackBar.open(`Permissions for ${user.name} set successfully!`, 'close', {duration: 3000});
     });
+  }
+
+  createNewCourse() {
+    this.db.createCourse(this.newCourse)
+      .then(() => {
+        this.snackBar.open(`Course ${this.newCourse.name} (${this.newCourse.id}) created successfully!`, 'close', {duration: 3000});
+      });
   }
 }
