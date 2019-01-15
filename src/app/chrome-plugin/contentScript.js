1f1c75a4c37406c7f1c25c6c6da00eac1d35a8fd
guyme <guy.menhel@gmail.com>
2019-01-15 15:50:29 +0200

    added an onclick event listener for the scan download button

--- src/app/chrome-plugin/contentScript.js
+++ src/app/chrome-plugin/contentScript.js
@@ -1,1 +1,7 @@
-alert('hello');
+let scan_buttons = document.getElementsByClassName("scan");
+
+for (let i = 0; i < scan_buttons.length; i++) {
+  scan_buttons[i].addEventListener('click', function(){
+    alert("clicked :-)");
+  });
+}
