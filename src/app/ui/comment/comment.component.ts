c38a8fd2128ec7a4719d4ccf91d8b8077fd3c462
Ron Marcovich <ron4548@gmail.com>
2019-01-18 16:17:50 +0200

    created comment component

---
+++ src/app/ui/comment/comment.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CommentId, CommentWithCreatorId} from '../../core/entities/comment';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss']
+})
+export class CommentComponent implements OnInit {
+
+  @Input()
+  comment: CommentWithCreatorId;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
