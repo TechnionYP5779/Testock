d09b93c1b1461ad55c9cf7e9462b175fb518b04e
Ron Marcovich <ron4548@gmail.com>
2019-01-18 16:19:39 +0200

    modified topic component to display topic and comments

--- src/app/ui/topic/topic.component.ts
+++ src/app/ui/topic/topic.component.ts
@@ -1,19 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {Topic} from '../../core/entities/topic';
 import {DbService} from '../../core/db.service';
-import {TopicId} from '../../core/entities/topic';
-import {Comment} from '../../core/entities/comment';
-import {CommentId} from '../../core/entities/comment';
-import {AuthService} from '../../core/auth.service';
-
-// Todo: temp class need to remove after db works:
-export class MyTopic implements Topic {
-  subject = 'my temp subject';
-  text = 'content';
-  creator = 'max calderon';
-  created = new Date();
-}
+import {TopicId, TopicWithCreatorId} from '../../core/entities/topic';
+import {Observable} from 'rxjs';
+import {flatMap, map, switchMap} from 'rxjs/operators';
+import {CommentWithCreatorId} from '../../core/entities/comment';
 
 @Component({
   selector: 'app-topic',
@@ -22,33 +13,16 @@ export class MyTopic implements Topic {
 })
 export class TopicComponent implements OnInit {
 
-  // Todo: this comment is the code needed after the DB works:
-  // public topic: Topic;
-  // Todo: temp class need to remove after DB works
-  public topic: MyTopic;
-  public id: number;
-  public creatorName: string;
+  private readonly id: string;
+  public topic$: Observable<TopicWithCreatorId>;
+  public comments$: Observable<CommentWithCreatorId[]>;
 
   constructor(private route: ActivatedRoute, private db: DbService) {
-    this.id = +this.route.snapshot.paramMap.get('id');
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.topic$ = this.db.getTopic(this.id);
+    this.comments$ = this.db.getTopic(this.id).pipe(switchMap(topic => this.db.getCommentsForTopic(topic.id)));
   }
 
   ngOnInit() {
-    this.getTopic();
-    this.getCreator();
-  }
-
-  getTopic(): void {
-    // Todo: this comment is the code needed after the DB works:
-    // this.db.getTopic(this.id).subscribe(topic => this.topic = topic);
-    // Todo: temp need to remove after DB works:
-    this.topic = new MyTopic();
-  }
-
-  getCreator(): void {
-    // Todo: this comment is the code needed after the DB works:
-    // this.db.getUser(this.topic.creator).subscribe(user => this.creatorName = user.uid);
-    // Todo: temp need to remove after DB works:
-    this.creatorName = 'max calderon';
   }
 }
