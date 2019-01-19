9224c81f4ed6887fe0cc58c104ba3b777417585c
Ron Marcovich <ron4548@gmail.com>
2019-01-19 21:46:49 +0200

    closes #178

--- src/app/ui/topic/topic.component.ts
+++ src/app/ui/topic/topic.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {DbService} from '../../core/db.service';
 import {TopicId, TopicWithCreatorId} from '../../core/entities/topic';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {flatMap, map, switchMap} from 'rxjs/operators';
 import {CommentWithCreatorId} from '../../core/entities/comment';
+import {Course} from '../../core/entities/course';
+import {QuestionId} from '../../core/entities/question';
 
 @Component({
   selector: 'app-topic',
@@ -16,11 +18,41 @@ export class TopicComponent implements OnInit {
   private readonly id: string;
   public topic$: Observable<TopicWithCreatorId>;
   public comments$: Observable<CommentWithCreatorId[]>;
+  public linkedCourse$: Observable<Course>;
+  public linkedQuestion$: Observable<QuestionId>;
+  public linkedQuestionCourse$: Observable<Course>;
 
   constructor(private route: ActivatedRoute, private db: DbService) {
     this.id = this.route.snapshot.paramMap.get('id');
     this.topic$ = this.db.getTopic(this.id);
     this.comments$ = this.db.getCommentsForTopic(this.id);
+    this.linkedCourse$ = this.db.getTopic(this.id).pipe(flatMap(topic => {
+      if (topic.linkedCourseId) {
+        return this.db.getCourse(topic.linkedCourseId);
+      } else {
+        return of(null);
+      }
+    }));
+    this.linkedQuestion$ = this.db.getTopic(this.id).pipe(flatMap(topic => {
+      if (topic.linkedQuestionId) {
+        return this.db.getQuestion(topic.linkedQuestionId);
+      } else {
+        return of(null);
+      }
+    }));
+    this.linkedQuestionCourse$ = this.db.getTopic(this.id).pipe(flatMap(topic => {
+      if (topic.linkedQuestionId) {
+        return this.db.getQuestion(topic.linkedQuestionId);
+      } else {
+        return of(null);
+      }
+    })).pipe(flatMap(question => {
+      if (question) {
+        return this.db.getCourse(question.course);
+      } else {
+        return of(null);
+      }
+    }));
   }
 
   ngOnInit() {
