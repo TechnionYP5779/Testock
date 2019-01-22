9955c67ef49c24b2c5893aa42452898200e346bf
Yuval Ron <yuvalron@hotmail.com>
2019-01-22 21:11:01 +0200

    #180 showing courses per faculty

--- src/app/ui/faculty/faculty.component.ts
+++ src/app/ui/faculty/faculty.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from '../../core/entities/course';
+import {DbService} from '../../core/db.service';
+import {ActivatedRoute} from '@angular/router';
+import {Faculty, FacultyId} from '../../core/entities/faculty';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-faculty',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacultyComponent implements OnInit {
 
-  constructor() { }
+  public faculty: FacultyId;
+  public courses: Course[];
+  public id: string;
 
-  ngOnInit() {
+  constructor(private afs: AngularFirestore, private route: ActivatedRoute, private db: DbService) {
+    this.id = this.route.snapshot.paramMap.get('id');
   }
 
+  ngOnInit() {
+    this.getFaculty();
+    this.getCourses();
+  }
+
+  private getCourses(): void {
+    this.db.getCoursesOfFaculty(this.id).subscribe(courses => this.courses = courses);
+  }
+
+  private getFaculty(): void {
+    this.db.getFaculty(this.id).subscribe(faculty => this.faculty = faculty);
+  }
 }
