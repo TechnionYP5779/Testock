6886d9c4d3fa5ea9b5e6165c671073b26d32d791
ameeramer <ameeramer@campus.technion.ac.il>
2019-04-09 00:03:00 +0300

    #215: moved ocr logic to a separate service

---
+++ src/app/core/ocr.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { OCRService } from './ocr.service';
+
+describe('OCRService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: OCRService = TestBed.get(OCRService);
+    expect(service).toBeTruthy();
+  });
+});
--- src/app/ui/upload/upload.component.ts
+++ src/app/ui/upload/upload.component.ts
@@ -6,9 +6,8 @@ import {UploadService} from '../../core/upload.service';
 import {MatSnackBar} from '@angular/material';
 import {ActivatedRoute} from '@angular/router';
 import {FacultyId} from '../../core/entities/faculty';
-import { HttpClient } from '@angular/common/http';
-import construct = Reflect.construct;
-import {log} from 'util';
+import {OCRService} from '../../core/ocr.service';
+
 
 @Injectable()
 class QuestionSolution {
@@ -16,6 +15,7 @@ class QuestionSolution {
   public images: string[];
   public grade: number;
   public points: number;
+
   constructor(index: number) {
     this.index = index;
     this.images = [];
@@ -52,13 +52,10 @@ export class UploadComponent implements OnInit {
 
   public blobs: Blob[];
   public isDragged: boolean;
-  public http: HttpClient;
 
-
-  constructor(private db: DbService, private pdf: PdfService, private uploadService: UploadService, public snackBar: MatSnackBar,
-              private route: ActivatedRoute, http: HttpClient) {
+  constructor(private db: DbService, private pdf: PdfService, private ocr: OCRService, private uploadService: UploadService, public snackBar: MatSnackBar,
+              private route: ActivatedRoute) {
     this.route = route;
-    this.http = http;
   }
 
   public uploadState = UploadState;
@@ -71,7 +68,7 @@ export class UploadComponent implements OnInit {
   public faculty: FacultyId;
 
   ngOnInit() {
-    const source = this.route.snapshot.paramMap.get('source')
+    const source = this.route.snapshot.paramMap.get('source');
     if (source) {
       if (source === 'chrome') {
         this.snackBar.open('Drag&Drop your scan from the bottom bar to the upload area', 'close', {duration: 3000});
@@ -84,21 +81,7 @@ export class UploadComponent implements OnInit {
     this.loadFile(file);
   }
 
-  private matchInArray(regex, expressions) {
-
-    const len = expressions.length;
-    let i = 0;
-
-    for (; i < len; i++) {
-      if (expressions[i].match(regex)) {
-        return expressions[i];
-      }
-    }
-
-    return '';
-  }
-
-  private getCourseDetailsByName(fileName: String){
+  private getCourseDetailsByName(fileName: String) {
     if (/^([0-9]{9}-20[0-9]{2}0([123])-[0-9]{6}-([123]))/.test(fileName.toString())) {
       const split = fileName.split('-');
       const courseId = parseInt(split[2], 10);
@@ -117,7 +100,7 @@ export class UploadComponent implements OnInit {
 
   private getCourseDetails(file: File) {
     const fileName = file.name;
-    try{
+    try {
       this.getCourseDetailsByName(fileName);
     } catch (e) {
       throw e;
@@ -125,49 +108,20 @@ export class UploadComponent implements OnInit {
   }
 
   private getCourseDetailsBySticker(firstPage: Blob) {
-    const p = new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      let base64data: any;
-      reader.readAsDataURL(firstPage);
-      reader.onloadend = function () {
-        base64data = reader.result;
-        resolve(base64data);
-      };
-    });
-    p.then(res => {
-      const json = {
-        'requests': [
-          {
-            'image': {
-              'content': res.toString().substr(23)
-            },
-            'features': [
-              {
-                'type': 'TEXT_DETECTION',
-                'maxResults': 10000
-              }
-            ]
-          }
-        ]
-      };
-      this.http.post('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBPgqzt5VUAa-Jqc5Qu9DSFcO851rj8p5Q', json)
-        .subscribe(result => {
-          const info = this.matchInArray(/^[/\d]{4}[.][/\d]{2}[-][/\d]{6}[-][/\d]$/,
-            result['responses'][0].textAnnotations.map((val, ind) => val['description']));
-          if (info !== '') {
-            const year = info.toString().substr(0,4);
-            const semester = info.toString().substr(5,2);
-            const number = info.toString().substr(8,6);
-            const moed = info.toString().substr(15,1);
-            const fileName = '000000000-' + year + semester + '-' + number + '-' + moed;
-            try{
-              this.getCourseDetailsByName(fileName);
-            } catch (e) {
-              this.snackBar.open('Invalid file name', 'close', {duration: 3000});
-              return;
-            }
-          }
-        });
+    this.ocr.getInfoFromSticker(firstPage).then(info => {
+        if (info !== '') {
+        const year = info.toString().substr(0, 4);
+        const semester = info.toString().substr(5, 2);
+        const number = info.toString().substr(8, 6);
+        const moed = info.toString().substr(15, 1);
+        const fileName = '000000000-' + year + semester + '-' + number + '-' + moed;
+        try {
+          this.getCourseDetailsByName(fileName);
+        } catch (e) {
+          this.snackBar.open('Invalid file name', 'close', {duration: 3000});
+          return;
+        }
+      }
     });
   }
 
@@ -208,9 +162,10 @@ export class UploadComponent implements OnInit {
 
   loadFile(file): void {
     this.resetForm();
-    try{
+    try {
       this.getCourseDetails(file);
-    } catch (e) {}
+    } catch (e) {
+    }
     this.questions = [];
     this.pdf.getImagesOfFile(file).then(res => {
       this.blobs = res;
