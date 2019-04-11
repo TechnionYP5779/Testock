8517eef0ea330b89a33caf857f958ceaa7605663
Ron Marcovich <ron4548@gmail.com>
2019-04-11 18:41:32 +0300

    removed unneccesary imports

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../core/auth.service';
 import {Observable} from 'rxjs';
 import {UserData} from '../../core/entities/user';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {MsGraphService} from '../../core/msgraph.service';
-import {MsalService} from '@azure/msal-angular';
 
 @Component({
   selector: 'app-user-profile',
