3add99985c28945d199643356d0619c44562c02d
Ron Marcovich <ron4548@gmail.com>
2019-04-13 11:30:15 +0300

    revised msgraph service to provide simple http requests

--- src/app/core/msgraph.service.ts
+++ src/app/core/msgraph.service.ts
@@ -2,37 +2,31 @@ import { Injectable } from '@angular/core';
 import {AuthService} from './auth.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {flatMap, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MsGraphService {
 
-  constructor(private auth: AuthService, private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-  get profilePictureAsBlob(): Promise<Blob> {
-    return this.auth.msToken.then(accessToken => {
-        const httpOptions = {
-          headers: new HttpHeaders({
-            'Authorization': 'Bearer ' + accessToken
-          }),
-          responseType: 'blob' as 'blob'
-        };
-        return this.http.get('https://graph.microsoft.com/v1.0/me/photos/120x120/$value', httpOptions).toPromise();
-      }
-    );
+  selfProfilePicture(accessToken: string): Observable<Blob> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + accessToken
+      }),
+      responseType: 'blob' as 'blob'
+    };
+    return this.http.get('https://graph.microsoft.com/v1.0/me/photos/64x64/$value', httpOptions);
   }
 
-  getProfilePictureForUserAsBlob(uid: string): Promise<Blob> {
-    return this.auth.msToken.then(accessToken => {
-        const httpOptions = {
-          headers: new HttpHeaders({
-            'Authorization': 'Bearer ' + accessToken
-          }),
-          responseType: 'blob' as 'blob'
-        };
-        return this.http.get(`https://graph.microsoft.com/v1.0/users/${uid}/photos/64x64/$value`, httpOptions).toPromise();
-      }
-    );
+  getFaculty(accessToken: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': 'Bearer ' + accessToken
+      })
+    };
+    return this.http.get('https://graph.microsoft.com/v1.0/me?$select=department', httpOptions);
   }
 }
