a998604a053141de0f7ac4afa3ad21eedb1c09f4
guyme <guy.menhel@gmail.com>
2019-04-19 12:50:13 +0300

    added empty game-planet component

---
+++ src/app/gamification/game-planet/game-planet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-planet',
+  templateUrl: './game-planet.component.html',
+  styleUrls: ['./game-planet.component.scss']
+})
+export class GamePlanetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/gamification/gamification.module.ts
+++ src/app/gamification/gamification.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameWorldComponent } from './game-world/game-world.component';
+import { GamePlanetComponent } from './game-planet/game-planet.component';
 
 @NgModule({
-  declarations: [GameWorldComponent],
+  declarations: [GameWorldComponent, GamePlanetComponent],
   imports: [
     CommonModule
   ]
