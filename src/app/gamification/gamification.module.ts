2607aa099ef8763664a09331535d6e535742245b
guyme <guy.menhel@gmail.com>
2019-04-22 18:49:28 +0300

    added empty gamification service

--- src/app/gamification/gamification.module.ts
+++ src/app/gamification/gamification.module.ts
@@ -5,6 +5,7 @@ import { GamePlanetComponent } from './game-planet/game-planet.component';
 import {Planet} from './entities/planet';
 import {AngularFontAwesomeModule} from 'angular-font-awesome';
 import {UsersModule} from '../users/users.module';
+import {GamificationService} from './gamification.service';
 
 const planets: Planet[] = [
   {
@@ -71,7 +72,8 @@ const planets: Planet[] = [
 @NgModule({
   declarations: [GameWorldComponent, GamePlanetComponent],
   providers: [
-    {provide: 'Planets', useValue: planets}
+    {provide: 'Planets', useValue: planets},
+    GamificationService
   ],
   imports: [
     CommonModule,
