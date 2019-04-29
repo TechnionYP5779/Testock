55ab215c8e83511663891cde299b1a1a09a96e3b
Yuval Ron <yuvalron@hotmail.com>
2019-04-29 14:25:11 +0300

    added personal progress pannel in user profile page

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -5,6 +5,8 @@ import {UserData} from '../../entities/user';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {ActivatedRoute} from '@angular/router';
 import {DbService} from '../../core/db.service';
+import {QuestionId} from '../../entities/question';
+import {Sort} from '@angular/material';
 
 @Component({
   selector: 'app-user-profile',
@@ -15,6 +17,7 @@ export class UserProfileComponent implements OnInit {
 
   user$: Observable<UserData>;
   userId: string;
+  public questions: QuestionId[];
 
   constructor(public auth: AuthService, private db: DbService, private route: ActivatedRoute) {
     this.userId = this.route.snapshot.paramMap.get('uid');
@@ -22,6 +25,38 @@ export class UserProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.getQuestions();
   }
+
+  getQuestions(): void {
+    this.db.getAllQuestions().subscribe(questions => this.questions = questions);
+  }
+
+  sortQuestions(sort: Sort) {
+    const data = this.questions;
+    if (!sort.active || sort.direction === '') {
+      return;
+    }
+    this.questions = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'course': return compare(a.course, b.course, isAsc);
+        case 'year': return compare(a.year, b.year, isAsc);
+        case 'semester': return compare(a.semester, b.semester, isAsc);
+        case 'moed': return compare(a.moed, b.moed, isAsc);
+        case 'number': return compare(a.number, b.number, isAsc);
+        // TODO: case 'difficulty': return compare(a.difficulty, b.difficulty, isAsc);
+        // TODO: case 'is_solved': return compare(a.is_solved, b.is_solved, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
+  remove() {
+    return;
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? -1 : 1);
 }
