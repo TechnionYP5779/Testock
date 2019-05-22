c19e16ef7dda01deb10489c3df27efe2bd916a85
Ron Marcovich <ron4548@gmail.com>
2019-05-22 23:28:00 +0300

    implemented the ui of pending solution crop

--- src/app/pending-scans/pending-scans.module.ts
+++ src/app/pending-scans/pending-scans.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { PendingScanComponent } from './pending-scan/pending-scan.component';
 import {CoreModule} from '../core/core.module';
 import { PendingSolutionModalComponent } from './pending-solution-modal/pending-solution-modal.component';
+import {ImageCropperModule} from 'ngx-image-cropper';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
   declarations: [PendingScanComponent, PendingSolutionModalComponent],
   imports: [
     CommonModule,
-    CoreModule
+    CoreModule,
+    ImageCropperModule,
+    AngularFontAwesomeModule
   ],
   exports: [
     PendingScanComponent,
--- src/app/pending-scans/pending-solution-modal/pending-solution-modal.component.ts
+++ src/app/pending-scans/pending-solution-modal/pending-solution-modal.component.ts
@@ -4,7 +4,8 @@ import {QuestionId} from '../../entities/question';
 import {SolutionId} from '../../entities/solution';
 import {DbService} from '../../core/db.service';
 import {PendingScanId} from '../../entities/pending-scan';
-import {Observable} from 'rxjs';
+import {take} from 'rxjs/operators';
+import {ImageCroppedEvent} from 'ngx-image-cropper';
 
 @Component({
   selector: 'app-pending-solution-modal',
@@ -16,13 +17,54 @@ export class PendingSolutionModalComponent implements OnInit {
   @Input() question: QuestionId;
   @Input() solution: SolutionId;
 
-  public pendingScan$: Observable<PendingScanId>;
+  public pendingScan: PendingScanId;
+  public cropMode: boolean;
+  public isPageCropped: boolean[];
+  public pagesBase64: string[];
+  public croppedPages: string[];
 
   constructor(public activeModal: NgbActiveModal, private db: DbService) {
   }
 
   ngOnInit() {
-    this.pendingScan$ = this.db.getPendingScan(this.solution.pendingScanId);
+    this.db.getPendingScan(this.solution.pendingScanId).pipe(take(1)).toPromise().then(ps => {
+      this.pendingScan = ps;
+      this.isPageCropped = Array(ps.pages.length).map(() => false);
+      this.croppedPages = Array(ps.pages.length).map(() => null);
+      Promise.all(ps.pages.map(pageUrl => this.getImageBase64(pageUrl))).then(res => this.pagesBase64 = res);
+    });
   }
 
+  enableCropMode() {
+    this.cropMode = true;
+  }
+
+  disableCropMode() {
+    this.cropMode = false;
+  }
+
+  imageClicked(i: number) {
+    if (!this.cropMode) { return; }
+    this.isPageCropped[i] = true;
+  }
+
+  getImageBase64(url: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const res: any = await fetch(url, {mode: 'cors'});
+      const blob = await res.blob();
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = () => reject(reader.error);
+      reader.readAsDataURL(blob);
+    });
+  }
+
+  updateCroppedPage(i: number, $event: ImageCroppedEvent) {
+    this.croppedPages[i] = $event.base64;
+  }
+
+  cancelPage(i: number) {
+    this.croppedPages[i] = null;
+    this.isPageCropped[i] = false;
+  }
 }
