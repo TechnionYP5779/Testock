24c0efa2c74342fb7f25784e1c17883cd36383b8
Ron Marcovich <ron4548@gmail.com>
2019-05-23 11:15:26 +0300

    Created pending scans list component

--- src/app/pending-scans/pending-scans.module.ts
+++ src/app/pending-scans/pending-scans.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PendingScanComponent } from './pending-scan/pending-scan.component';
 import {CoreModule} from '../core/core.module';
+import { PendingScansListComponent } from './pending-scans-list/pending-scans-list.component';
+import {AppRoutingModule} from '../app-routing.module';
 
 @NgModule({
-  declarations: [PendingScanComponent],
+  declarations: [PendingScanComponent, PendingScansListComponent],
   imports: [
     CommonModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule
   ],
   exports: [
-    PendingScanComponent
+    PendingScansListComponent
   ]
 })
 export class PendingScansModule { }
