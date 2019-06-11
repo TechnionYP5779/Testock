631b4d4fd3067cc0ea7a19ca39de7ac0edf3b7ff
Guy Menhel <guy.menhel@gmail.com>
2019-06-11 17:33:36 +0300

    fix click on notification (now marks notification as seen)

--- src/app/notifications/notifications-list/notifications-list.component.ts
+++ src/app/notifications/notifications-list/notifications-list.component.ts
@@ -23,11 +23,15 @@ export class NotificationsListComponent implements OnInit {
     );
   }
 
-  toggleSeen(event: MouseEvent, notification: NotificationId, redirect: boolean) {
+  changeSeen(event: MouseEvent, notification: NotificationId, seen: boolean, redirect: boolean = true) {
     if (redirect === false) {
       event.preventDefault();
       event.stopPropagation();
     }
     this.notifications.updateNotificationState(notification.id, !notification.seen);
+  }
+
+  toggleSeen(event: MouseEvent, notification: NotificationId, redirect: boolean) {
+    this.changeSeen(event, notification, !notification.seen, redirect);
   }
 }
