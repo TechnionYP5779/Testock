5345eafe3ba3aea3b07593ce0c4698e547e1d637
Ron Marcovich <ron4548@gmail.com>
2019-06-12 09:01:46 +0300

    removed unused import

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SemesterPipe} from './semester.pipe';
 import {SafeUrlPipe} from './safe-url.pipe';
-import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {DbService} from './db.service';
 import {OCRService} from './ocr.service';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
