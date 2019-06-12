0f700fd3408626c61fa794d9905042f6d3c6a6d0
Guy Menhel <guy.menhel@gmail.com>
2019-06-12 18:42:07 +0300

    Close popover when notification is clicked

--- src/app/notifications/notifications-list/notifications-list.component.ts
+++ src/app/notifications/notifications-list/notifications-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NotificationId} from '../../entities/notification';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
@@ -14,6 +14,7 @@ import {UserData} from '../../entities/user';
 export class NotificationsListComponent implements OnInit {
   @Input() user: Observable<UserData>;
   public notifications$: Observable<NotificationId[]>;
+  @Output() notificationClicked = new EventEmitter<NotificationId>();
 
   constructor(private notifications: NotificationsService) { }
 
@@ -29,6 +30,10 @@ export class NotificationsListComponent implements OnInit {
       event.stopPropagation();
     }
     this.notifications.updateNotificationState(notification.id, seen);
+
+    if (redirect === true) {
+      this.notificationClicked.emit(notification);
+    }
   }
 
   toggleSeen(event: MouseEvent, notification: NotificationId, redirect: boolean) {
