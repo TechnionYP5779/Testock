9911fe2bfcc44671650825296129d3784c427869
Ron Marcovich <ron4548@gmail.com>
2019-06-12 17:29:34 +0300

    pages are now extracted to exactly replicate the PDF uploaded

--- src/app/upload/pdf.service.ts
+++ src/app/upload/pdf.service.ts
@@ -9,55 +9,45 @@ const PDFJS = pdfjsLib;
 })
 export class PdfService {
 
+  readonly PAGES_EXPORT_WIDTH = 1700;
+
   constructor() { }
 
-  public async getBlobsOfPDF(file: string|Uint8Array): Promise<Blob[]> {
-
+  private async getPDFPages(file: string|Uint8Array): Promise<ScanPage[]> {
     const doc = await PDFJS.getDocument(file);
-    const images = [];
+    const canvas = document.createElement('canvas');
+    const pages = [];
 
     for (let i = 1; i <= doc.numPages; ++i) {
       const page = await doc.getPage(i);
-      const ops = await page.getOperatorList();
+      let viewport = page.getViewport(1);
+      viewport = page.getViewport(this.PAGES_EXPORT_WIDTH / viewport.width);
+      const context = canvas.getContext('2d');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      await page.render({canvasContext: context, viewport: viewport});
 
-      for (let j = 0; j < ops.fnArray.length; j++) {
-        if (ops.fnArray[j] === PDFJS.OPS.paintJpegXObject) {
-          images.push(page.objs.get((ops.argsArray[j][0])));
-        }
-      }
+      const base64 = canvas.toDataURL('image/jpeg');
+      const blob = await new Promise<Blob>(resolve => {
+        canvas.toBlob(newBlob => resolve(newBlob), 'image/jpeg');
+      });
 
+      pages.push(new ScanPage(i, blob, base64));
     }
 
-    const res = [];
-    for (let i = 0; i < images.length; ++i) {
-      res.push(fetch(images[i].src).then(r => r.blob()));
-    }
-
-    return Promise.all(res);
-
+    return pages;
   }
 
-  public async getScanPagesOfPDF(file: File): Promise<ScanPage[]> {
+  public getScanPagesOfPDF(file: File): Promise<ScanPage[]> {
 
     return new Promise((resolve, reject) => {
       const fr = new FileReader();
       fr.onload = () => resolve(fr.result);
-      fr.onerror = () => reject();
+      fr.onerror = () => reject(fr.error);
       fr.readAsArrayBuffer(file);
     }).then(async res => {
       const data = new Uint8Array(res as ArrayBuffer);
-      const blobs = await this.getBlobsOfPDF(data);
-      const base64 = await Promise.all(blobs.map(blob => getImageBase64FromBlob(blob)));
-      return blobs.map((blob, i) => new ScanPage(i + 1, blob, base64[i]));
+      return this.getPDFPages(data);
     });
   }
-}
-
-function getImageBase64FromBlob(blob: Blob): Promise<string> {
-  return new Promise(async (resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = () => reject(reader.error);
-    reader.readAsDataURL(blob);
-  });
 }
