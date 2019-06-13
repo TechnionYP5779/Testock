72808ada4d7abb068b89ee6cb06b86532a06755a
Ron Marcovich <ron4548@gmail.com>
2019-06-13 18:37:00 +0300

    firebase config is now secret and safe

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
+import {firebase_config} from './firebase/firebase-config';
+
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyD-2Kzoolfyx1sYaIeJDb_5jvf7_Ef00m4',
-    authDomain: 'testock-europe.firebaseapp.com',
-    databaseURL: 'https://testock-europe.firebaseio.com',
-    projectId: 'testock-europe',
-    storageBucket: 'testock-europe.appspot.com',
-    messagingSenderId: '214842256227',
-    appId: '1:214842256227:web:8f49974022bcf825',
-    functionsLocation: 'europe-west1'
-  }
+  firebase: firebase_config
 };
