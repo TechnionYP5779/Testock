634a2a72df1222a62b5f62e823379195d5b29cd5
Ron Marcovich <ron4548@gmail.com>
2019-06-20 18:43:49 +0300

    added toast when resetting an invalid grade

--- src/app/upload/scan-editor/question-solution/question-solution.component.ts
+++ src/app/upload/scan-editor/question-solution/question-solution.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {QuestionSolution, QuestionType} from '../question-solution';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import {SolutionImage} from '../solution-image';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-question-solution',
@@ -21,18 +22,25 @@ export class QuestionSolutionComponent implements OnInit {
 
   QuestionType = QuestionType;
 
-  constructor() {}
+  constructor(private snackBar: MatSnackBar) {}
 
   ngOnInit() {
   }
 
   verifyGrade() {
+    let error = null;
     if (this.sol.grade < 0) {
       this.sol.grade = 0;
+      error = 'Grade must be a non-negative number';
     }
 
     if (this.sol.grade > this.sol.points) {
       this.sol.grade = this.sol.points;
+      error = 'Grade can not be greater than question\'s total grade';
+    }
+
+    if (error) {
+      this.snackBar.open('Error: ' + error, 'close', {duration: 3000});
     }
   }
 
